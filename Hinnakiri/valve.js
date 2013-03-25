75d0f9de6af92f311457de0f6c437c1b93eb1187
Erko Pyi <erko@yoga.ee>
2013-03-25 21:47:21 +0200

    update

--- Hinnakiri/valve.js
+++ Hinnakiri/valve.js
@@ -1,10 +1,11 @@
 function hinnakiri(s){
-var valve = {
+var Valve = {
 	central:[
-		{name: "Paradox", price:100, zone:6, type:"paradox"},
-		{name: "Dsc", price:110, zone:6, type:"DSC"}],
+		{name: "Paradox", price:100, zone:6, family:"paradox"},
+		{name: "Dsc", price:110, zone:6, type:"DSC"}
+	],
 	keypad:[
-		{type: LCD, price:100, supported:["paradox"]},
+		{type: "LCD", price:100, family:["paradox"]}
 	],
 	sensors:[
 		{type: "motion", price: 10},
@@ -12,22 +13,14 @@ var valve = {
 		{type: "glass", price: 12},
 		{type: "smoke", price: 13},
 		{type: "heat", price: 14},
-		{type: "water", price: 15}],
+		{type: "water", price: 15}
+	],
 	sirens:[
 		{type: "indoor", price: 10},
-		{type: "outdoor", price: 20}]
+		{type: "outdoor", price: 20}
+	]
 }
 
-var inside_html = liquid("
-<ul class='products'>
-	{% for x in sensors %}
-		<li>
-			<h2>{{ x.type }}</h2>
-			Price: {{ x.price }}
-		</li>
-	{% endfor %}
-</ul>
-")
-		console.log("DEBUG");
-		document.getElementById(s).innerHTML = inside_html(valve);
+	var inside_html = liquid(document.getElementById("products").innerHTML)
+	document.getElementById(s).innerHTML = inside_html(Valve);
 }
